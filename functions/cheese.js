e49a5af9db44bf385a4a6195d6ac9119178dba3b
traceannwhite <traceannwhite@gmail.com>
2021-08-17 10:01:20 -0500

    sf

--- functions/cheese.js
+++ functions/cheese.js
@@ -3,6 +3,10 @@ exports.handler = async function (event, context) {
   //Fetch request details from event object
   const { path, httpMethod, headers, queryStringParameters, body } = event;
 
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+
+  const data = await response.json();
+
   // return some JSON data with a status of 200
   return {
     statusCode: 200,
@@ -14,7 +18,7 @@ exports.handler = async function (event, context) {
       httpMethod,
       headers,
       queryStringParameters,
-      body: body ? JSON.parse(body) : "none",
+      body: data,
     }),
   };
 };
